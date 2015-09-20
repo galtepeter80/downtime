833751464ca9ec78512adc47a02a2565bb479b47
Gregory Altepeter <g.altepeter@gmail.com>
2015-09-20 09:08:24 -0500

    Update comment_template.php
    adds javascript functionality for dice

--- comment_template.php
+++ comment_template.php
@@ -22,4 +22,40 @@
             <button type="submit" class="btn btn-default">Submit!</button>
         </div>
 		</fieldset>
+		
 	</form>
+	
+	<script>
+		// adds dice-rolling functionality to the OOC window
+		function dice(number, size) {
+			var total = 0;
+			var type = " " + number + "D" + size + " (";
+			document.getElementById("ooc_form").value += type;
+			for (i = 0; i < number; i++) {
+				var roll = Math.floor((Math.random() * size) + 1);
+				document.getElementById("ooc_form").value += " ";
+				document.getElementById("ooc_form").value += roll;
+				total += roll;
+			}
+			document.getElementById("ooc_form").value += " ) = ";
+			document.getElementById("ooc_form").value += total;
+			//return roll;
+		}
+	</script>
+	
+	<! an array of dice buttons>
+	<button onclick="dice(1, 4)">1D4</button>
+	<button onclick="dice(1, 6)">1D6</button>
+	<button onclick="dice(2, 6)">2D6</button>
+	<button onclick="dice(3, 6)">3D6</button>
+	<button onclick="dice(4, 6)">4D6</button>
+	<button onclick="dice(1, 8)">1D8</button>
+	<button onclick="dice(2, 8)">2D8</button>
+	<button onclick="dice(1, 10)">1D10</button>
+	<button onclick="dice(2, 10)">2D10</button>
+	<button onclick="dice(3, 10)">3D10</button>
+	<button onclick="dice(1, 12)">1D12</button>
+	<button onclick="dice(1, 20)">1D20</button>
+	<br><br>
+
+	

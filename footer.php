b6a4dccd95130e3d603d462577907199ab1503f7
Gregory Altepeter <g.altepeter@gmail.com>
2015-09-20 08:46:47 -0500

    Create footer.php
    the standard footer file

---
+++ footer.php
@@ -0,0 +1,26 @@
+		</div>
+			<div>
+				<img alt="Divider" class="div_middle" src="../public/images/divider.png"/>
+			</div>
+			
+			<br><br>
+			
+		<div id="sitenav">
+		<a href="../public/index.html">Home</a>
+		&nbsp; | &nbsp;
+		<a href="../public/register.php">Register</a>
+		&nbsp; | &nbsp;
+		<a href="../public/login.php">Log In</a>
+		&nbsp; | &nbsp;
+		<a href="../public/campaign.php">My Campaign</a>
+		&nbsp; | &nbsp;
+		<a href="../public/logout.php">Logout</a>
+		</div>
+			
+		<div id="bottom">
+			DOWNTIME is copyright 2015 by Greg Altepeter. Please use it as you see fit for your own personal gaming fun, but be respectful. 
+		</div>
+			
+	</body>
+	
+</html>
